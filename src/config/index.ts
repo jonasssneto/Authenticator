f5c048349ccb94cc1f5b5fa3d94435ed880d9623
Jonas Nogueira Neto <44125908+jonasssneto@users.noreply.github.com>
2024-04-16 00:00:28 -0300

    chore: env config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,14 +2,17 @@ import "dotenv/config";
 import z from "zod";
 
 const zod = z.object({
-    PORT: z.string(),
-    CORS_ORIGIN: z.string(),
-    DATABASE_URL: z.string(),
+  PORT: z.string(),
+  CORS_ORIGIN: z.string(),
+  DATABASE_URL: z.string(),
+  SMTP_HOST: z.string(),
+  SMTP_PORT: z.coerce.number(),
+  SMTP_USERNAME: z.string(),
+  SMTP_PASSWORD: z.string(),
+  SMTP_SENDER: z.string(),
 });
 
 const CONFIG = zod.parse(process.env);
 
-export {
-    CONFIG
-};
+export { CONFIG };
 
